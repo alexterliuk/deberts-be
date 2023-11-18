7ac637a559d782002ba9fc827da897610720809e
Alex Terliuk <alex.terliuk@gmail.com>
2023-11-18 10:33:18 +0200

    feat: add schema for creating a game

--- src/client/actions/schemas.ts
+++ src/client/actions/schemas.ts
@@ -1,15 +1,20 @@
 import Joi from 'joi';
-import { CARD_NAMES, SUIT_NAMES } from '../data';
+import {
+  CARD_NAMES,
+  SUIT_NAMES,
+  MIN_PLAYERS_QTY,
+  MAX_PLAYERS_QTY,
+} from '../data';
 
 // CardFaceType
 const cardFaceSchema = Joi.object({
-  name: Joi.valid(CARD_NAMES).required(),
-  suit: Joi.valid(SUIT_NAMES).required(),
+  name: Joi.valid(...CARD_NAMES).required(),
+  suit: Joi.valid(...SUIT_NAMES).required(),
 }).required();
 
 const cardFaceAndRanksForCombinationSchema = Joi.object({
-  name: Joi.valid(CARD_NAMES).required(),
-  suit: Joi.valid(SUIT_NAMES).required(),
+  name: Joi.valid(...CARD_NAMES).required(),
+  suit: Joi.valid(...SUIT_NAMES).required(),
   rank: Joi.number().integer().min(1).max(8).required(),
 }).required();
 
@@ -31,20 +36,20 @@ export const swapCardsSchema = Joi.object({
 
 // SuggestSuitActionType
 export const suggestSuitSchema = Joi.object({
-  type: Joi.string().equal('SUGGEST_SUIT').required(),
-  suit: Joi.string().valid(SUIT_NAMES).required(),
+  type: Joi.equal('SUGGEST_SUIT').required(),
+  suit: Joi.valid(...SUIT_NAMES).required(),
   playerIndex: playerIndexSchema,
 });
 
 // DeclareBellaActionType
 export const declareBellaSchema = Joi.object({
-  type: Joi.string().equal('DECLARE_BELLA').required(),
+  type: Joi.equal('DECLARE_BELLA').required(),
   playerIndex: playerIndexSchema,
 });
 
 // TradeCombinationsActionType
 export const tradeCombinationsSchema = Joi.object({
-  type: Joi.string().equal('TRADE_COMBINATIONS').required(),
+  type: Joi.equal('TRADE_COMBINATIONS').required(),
   records: Joi.array().items(
     Joi.object({
       playerIndex: playerIndexSchema,
@@ -52,3 +57,9 @@ export const tradeCombinationsSchema = Joi.object({
     }),
   ),
 });
+
+export const createGameSchema = Joi.array()
+  .items(Joi.string())
+  .min(MIN_PLAYERS_QTY)
+  .max(MAX_PLAYERS_QTY)
+  .unique();
